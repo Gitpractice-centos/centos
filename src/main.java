05bd4f0b463ef1e6ce38b868b3c6bdf957ba238c
sudheer554 <sudheer.ece2008@gmail.com>
2018-02-14 02:53:51 +0000

    mai.java

--- src/main.java
+++ src/main.java
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 this is java file from centos 172.31.28.252
 iam continuing
 this isew line
 testing
-=======
-this is new script
+is is new script
 new linew
->>>>>>> 385dfcdb906fd9f4d26715c973410e8d713fa38e
+################################
+additional line
